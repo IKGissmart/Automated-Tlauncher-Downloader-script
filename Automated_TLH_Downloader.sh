1e37d356f996633eac2d4dff5c60704d817fe42d
IKGissmart <84079013+IKGissmart@users.noreply.github.com>
2021-09-14 21:21:36 +0500

    Update Automated_TLH_Downloader.sh

--- Automated_TLH_Downloader.sh
+++ Automated_TLH_Downloader.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-cd
-cd Downloads
+cd ~/Downloads
 mkdir Tlauncher 
 cd Tlauncher 
-
 wget https://tlauncher.org/jar -O Tlauncher_EXTRACT_ME
 unzip Tlauncher_EXTRACT_ME
 echo "Mission is Done "
