28c1c4970acf2bd0dfea3e2b1a7fcc9a2f745034
IKGissmart <84079013+IKGissmart@users.noreply.github.com>
2021-09-15 12:19:35 +0500

    Update Automated_TLH_Downloader.sh

--- Automated_TLH_Downloader.sh
+++ Automated_TLH_Downloader.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/bin/bash 
 cd ~/Downloads
 mkdir Tlauncher 
 cd Tlauncher 
 wget https://tlauncher.org/jar -O Tlauncher_EXTRACT_ME
+echo "Now Downloading Liberica Standard JDK 16.0.2+7"
+wget https://download.bell-sw.com/java/16.0.2+7/bellsoft-jdk16.0.2+7-linux-amd64.tar.gz -O Liberica_Standard_JDK_16.0.2+7
 unzip Tlauncher_EXTRACT_ME
+unzip Liberica_Standard_JDK_16.0.2+7
 echo "Mission is Done "
 exit 
