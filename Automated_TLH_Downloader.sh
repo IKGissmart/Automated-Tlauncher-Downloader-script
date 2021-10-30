62700ad2b0187a461c34ef9233d0de401cb02e87
IKGissmart <84079013+IKGissmart@users.noreply.github.com>
2021-10-30 14:50:28 +0500

    Update Automated_TLH_Downloader.sh

--- Automated_TLH_Downloader.sh
+++ Automated_TLH_Downloader.sh
@@ -5,8 +5,8 @@ cd Tlauncher
 echo -e "\e[1;32mDownloading Tlauncher\e[m"
 wget https://tlauncher.org/jar -O Tlauncher_EXTRACT_ME
 echo -e "\e[1;32mNow Downloading Liberica Standard JDK 16.0.2+7\e[m"
-wget https://download.bell-sw.com/java/16.0.2+7/bellsoft-jdk16.0.2+7-linux-amd64.tar.gz -O Liberica_Standard_JDK_16
+wget https://cdn.azul.com/zulu/bin/zulu17.30.15-ca-jdk17.0.1-linux_x64.tar.gz -O AZUL_ZULU_JDK_17
 unzip Tlauncher_EXTRACT_ME
-tar -xzf Liberica_Standard_JDK_16
+tar -xzf AZUL_ZULU_JDK_17
 echo -e "\e[1;32mDONE\e[m"
 exit 
